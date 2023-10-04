43bede01d0760ac5a517ae8a1b4f921c9760e4bb
zulfamubina21 <139990907+zulfamubina21@users.noreply.github.com>
2023-10-04 20:25:40 +0700

    Delete config/sequelize.js

--- config/sequelize.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-
-dotenv.config()
-
-const sequelize = new Sequelize ("mysql://root:NlzGH6KDV7kY7kZKwFYd@containers-us-west-182.railway.app:5672/railway");
-
-export default sequelize;
